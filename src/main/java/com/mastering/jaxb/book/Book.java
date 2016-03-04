06ee047123236781399cc86d0aaeae7a32a715b0
alexandregama <alexandre.gama.lima@gmail.com>
2016-03-04 18:33:40 -0300

    Removes @XmlAccessorType from Book class

--- src/main/java/com/mastering/jaxb/book/Book.java
+++ src/main/java/com/mastering/jaxb/book/Book.java
@@ -1,12 +1,9 @@
 package com.mastering.jaxb.book;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
-@XmlAccessorType(XmlAccessType.PROPERTY)
 public class Book {
 
 	private Long id;
